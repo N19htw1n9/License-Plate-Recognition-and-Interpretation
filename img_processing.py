c34d77fb6a7ed489f49e613086e32c6cacad72d3
Sarim Qureshi <52934393+N19htw1n9@users.noreply.github.com>
2021-11-11 13:26:58 -0600

    Added link

--- img_processing.py
+++ img_processing.py
@@ -1,1 +1,13 @@
 # Assigned to Zak, Tosan, and Sarim
+
+'''
+Links:
+https://www.geeksforgeeks.org/license-plate-recognition-with-opencv-and-tesseract-ocr/
+
+https://medium.com/programming-fever/license-plate-recognition-using-opencv-python-7611f85cdd6c - main reference
+
+https://www.pyimagesearch.com/2020/09/21/opencv-automatic-license-number-plate-recognition-anpr-with-python/
+
+https://youtu.be/0-4p_QgrdbE
+
+'''
