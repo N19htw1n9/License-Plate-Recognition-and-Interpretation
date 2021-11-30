408f4ef6a43c0a5c7a4e9a1d65ca0fbf3bad08b7
Sarim Qureshi <52934393+N19htw1n9@users.noreply.github.com>
2021-11-29 21:49:17 -0600

    Added Tesseract stuff

--- img_processing.py
+++ img_processing.py
@@ -9,8 +9,8 @@ https://youtu.be/0-4p_QgrdbE
 '''
 
 import cv2
-# import pytesseract
-# pytesseract.pytesseract.tesseract_cmd = r'C:\ProgramFiles(x86)\Tesseract-OCR\tesseract.exe'
+import pytesseract
+pytesseract.pytesseract.tesseract_cmd = r"C:\Users\sarim\AppData\Local\Programs\Tesseract-OCR\tesseract.exe"
 
 def plate_recognition(path):
     img = cv2.imread(path)
@@ -38,19 +38,21 @@ def plate_recognition(path):
     threshold, outImg = cv2.threshold(plate, 80, 255, cv2.THRESH_BINARY)
     cv2.imshow("License Plate Detection", outImg)
     cv2.waitKey(0)
+    return img
 
 def plate_to_text(img):
     # Maybe use the Medium website for this instead, it's only two lines of code from there
+    # text = pytesseract.image_to_string(img, config='--psm 11')
     
-    '''
-    text = pytesseract.image_to_string(blackAndWhiteImage, config='--psm 6')
+    text = pytesseract.image_to_string(img, config='--psm 6')
     image = cv2.rectangle(image, (x,y), (x+w, y+h), (255, 0, 0) 3)
     image = cv2.putText(image, text.upper(), (x+50, y-10), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2, cv2.LINE_AA)
 
     print("License Plate: ", text.upper())
     cv2.imshow("License Plate Detection", image)
     cv2.waitKey(0)
-    '''
+    
 
 # Temporarily running function
-plate_recognition("test_pics/il7.jpg")
+img = plate_recognition("test_pics/il2.jpeg")
+print(plate_to_text(img))
