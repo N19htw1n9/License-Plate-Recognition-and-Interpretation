a4a036686836163913e24943695018fc38c6c1d8
Sarim Qureshi <52934393+N19htw1n9@users.noreply.github.com>
2021-12-02 14:15:06 -0600

    Slightly altered

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
 # Driver program meant to tie img_processing.py and automation_script.py together
+
+from automation_script import return_info
+
+return_info("AE29185")
+# return_info(plate_to_text(plate_recognition(path)))
